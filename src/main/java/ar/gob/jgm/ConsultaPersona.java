83dfdb5428b8d1d1d753644c006bf8211346a1c5
Federico J. Botti <fbotti@gmail.com>
2014-03-04 18:06:00 -0300

    Adds the DELETE, POST method and polish the GET Method.

--- src/main/java/ar/gob/jgm/ConsultaPersona.java
+++ src/main/java/ar/gob/jgm/ConsultaPersona.java
@@ -6,7 +6,9 @@ package ar.gob.jgm;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -22,11 +24,23 @@ public class ConsultaPersona {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("user/{username}")
-	public Map<String, Object> getPersonaCuilJSON(@PathParam("username") String username){
-		Map<String, Object> query = new HashMap<String, Object>();  
-		query.put("username", username);
-		return query; 
+	@Path("users")
+	public String getPersonaCuilJSON(){
+		return ""; 
+	}
+	
+	@DELETE
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("users")
+	public String deletePersonaCuilJSON(){
+		return ""; 
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("users")
+	public String postPersonaCuilJSON(){
+		return ""; 
 	}
 }
 
